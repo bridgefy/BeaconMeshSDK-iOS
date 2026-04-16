e50bcd95635db286c779cc909a3ff2ec4d895dfc
Francisco <francisco@bridgefy.me>
2026-04-16 10:53:41 -0600

    chore: Update sdk-ios package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/bridgefy/sdk-ios.git", from: "1.3.3"),
+        .package(url: "https://github.com/bridgefy/sdk-ios.git", from: "1.3.4"),
         .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", from: "2.0.0")
     ],
     
